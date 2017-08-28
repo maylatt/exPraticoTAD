45676da8f0ee2ad3d27bbf9272411fc7377c140d
maylatt <maylateixeira2013@gmail.com>
2017-08-27 23:08:57 -0300

    Implements conversion from color to gray.

--- TAD_ppm2pgm.c
+++ TAD_ppm2pgm.c
@@ -16,27 +16,49 @@ ppm_image * ler_ppm(const char *filename) {
  fscanf(img,"%2s", imgInput->format );
  fscanf(img, "%u", &imgInput->w);
  fscanf(img, "%u", &imgInput->h);
- fscanf(img, "%c", &imgInput->m);
+ fscanf(img, "%d", &imgInput->m);
 
- printf("format:%2s\nw:%u\nh:%u \nm:%d\n", imgInput->format, imgInput->w,imgInput->h,imgInput->m);
+ printf("formato:%2s\nw:%u\nh:%u \nm:%d\n", imgInput->format, imgInput->w, imgInput->h, imgInput->m);
 
- ppm_pixel pix[imgInput->h][imgInput->w];
+ imgInput->matrix = (ppm_pixel **)malloc((imgInput->h)*sizeof(ppm_pixel *));
 
 for(int r = 0 ;r < (imgInput->h); r++){
-   printf("\n");
+  imgInput->matrix[r] = (ppm_pixel *)malloc((imgInput->w)*sizeof(ppm_pixel));
    for(int c = 0 ; c < (imgInput->w); c++){
-     fscanf(img,"%c %c %c", &pix[r][c].r, &pix[r][c].g, &pix[r][c].b);
-     printf("%d %d %d  ", (pix[r][c].r), pix[r][c].g, pix[r][c].b );
+     fscanf(img,"%d %d %d", &imgInput->matrix[r][c].r, &imgInput->matrix[r][c].g, &imgInput->matrix[r][c].b);
+     //printf("%d %d %d ", imgInput->matrix[r][c].r, imgInput->matrix[r][c].g, imgInput->matrix[r][c].b);
    }
  }
 
- imgInput->matrix = &pix;
  fclose(img);
  return imgInput;
 }
 
 // Implemente a função para converter a imagem colorida PPM para a imagem em escala de cinzas PGM
 pgm_image * ppm_para_pgm(const ppm_image *ppm) {
+
+  pgm_image *imgOutput;
+
+  imgOutput = (pgm_image *)malloc(sizeof(pgm_image));
+
+  imgOutput->format[0]= 'P';
+  imgOutput->format[1]= '2';
+  imgOutput->format[2]= '\0';
+  imgOutput->w = ppm->w;
+  imgOutput->h = ppm->h;
+  imgOutput->m = 49;
+
+  imgOutput->matrix = (pgm_pixel **)malloc((imgOutput->h)*sizeof(pgm_pixel *));
+
+ for(int r = 0 ;r < (imgOutput->h); r++){
+   imgOutput->matrix[r] = (pgm_pixel *)malloc((imgOutput->w)*sizeof(pgm_pixel));
+    for(int c = 0 ; c < (imgOutput->w); c++){
+      imgOutput->matrix[r][c].p = (int)((0.192156863)*(0.30*ppm->matrix[r][c].r + 0.59*ppm->matrix[r][c].g + 0.11*ppm->matrix[r][c].b));
+      printf("%d", imgOutput->matrix[r][c].p);
+    }
+  }
+
+  return imgOutput;
 
 
 }
@@ -45,5 +67,19 @@ pgm_image * ppm_para_pgm(const ppm_image *ppm) {
 
 // Implemente a função para escrever a imagem PGM no arquivo
 void escreve_pgm(const char *filename, const pgm_image *pgm) {
+  FILE *img;
+  img = fopen(filename, "w");
+  fprintf(img, "%.2s\n", pgm->format);
+  fprintf(img, "%u ", pgm->w);
+  fprintf(img, "%u\n", pgm->h);
+  fprintf(img, "%d\n", pgm->m);
+
+  for(int r = 0 ;r <  (pgm->h); r++){
+     for(int c = 0 ; c < (pgm->w); c++){
+       fprintf(img, "%d ", pgm->matrix[r][c].p);
+     }
+     fprintf(img, "\n");
+   }
+   fclose(img);
 
 }
